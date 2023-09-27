6d0b51af1b9cb616312705ca0194fe9d7406d1b3
杨朗 <yanglang21@mails.tsinghua.edu.cn>
2023-09-27 19:47:42 +0800

    better instructure for null-ls.nvim

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,11 +1,36 @@
+-- NOTE: If you want to add more linting/formating tools, you can add them here.
 return {
+  -- First, add them to the ensure_installed list of mason.nvim
+  {
+    "mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "prettierd",
+        "latexindent",
+        "autopep8",
+        -- "cpplint",
+      })
+    end,
+  },
+
+  -- Then, add them to the sources list of null-ls.nvim
   {
     "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_,opts)
+    opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-      table.insert(opts.sources, nls.builtins.formatting.latexindent)
-      -- table.insert(opts.sources, nls.builtins.diagnostics.cpplint)
+      opts.sources = opts.sources or {}
+
+      ---------------------------------------------------
+      -- For linters, use nls.builtins.diagnostics.*   --
+      -- For formatters, use nls.builtins.formatting.* --
+      ---------------------------------------------------
+      vim.list_extend(opts.sources, {
+        nls.builtins.formatting.prettierd,
+        nls.builtins.formatting.latexindent,
+        nls.builtins.formatting.autopep8,
+        -- nls.builtins.diagnostics.cpplint,
+      })
     end,
   },
 }
